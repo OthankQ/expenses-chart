0ba0d9149c971305bb130ad90ee94c8ef9f4ce05
Ryan Kim <ryank9462@gmail.com>
2022-05-25 22:41:44 -0500

    add hover tooltip box reveal

---
+++ src/components/Bar/Bar.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: hsl(33, 100%, 98%);
+  gap: 5px;
+  padding-bottom: 10px;
+  span {
+    font-size: 0.8rem;
+    color: hsl(28, 10%, 53%);
+    background-color: hsl(33, 100%, 98%);
+  }
+`;
+
+export const StyledBar = styled.div`
+  height: 70px;
+  width: 30px;
+  background-color: hsl(10, 79%, 65%);
+  border-radius: 4px;
+  :hover {
+    background-color: hsl(186, 34%, 60%);
+    cursor: pointer;
+  }
+`;
+
+export const StyledTooltip = styled.div`
+  width: 100%;
+  height: 22px;
+  background-color: hsl(25, 47%, 15%);
+  padding: 2px 3px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  span {
+    color: hsl(33, 100%, 98%);
+    background-color: hsl(25, 47%, 15%);
+  }
+`;
+
+type BarProps = {
+  data: { day: string; amount: number };
+};
+
+type BarState = {
+  displayValue: string;
+};
+
+export class Bar extends React.Component<BarProps, BarState> {
+  constructor(props: BarProps) {
+    super(props);
+    this.state = {
+      displayValue: '0',
+    };
+  }
+
+  render() {
+    return (
+      <StyledContainer>
+        <StyledTooltip style={{ opacity: this.state.displayValue }}>
+          <span>${this.props.data.amount}</span>
+        </StyledTooltip>
+        <StyledBar
+          onMouseEnter={(e) => {
+            this.setState({ displayValue: '100' });
+          }}
+          onMouseLeave={(e) => {
+            this.setState({ displayValue: '0' });
+          }}
+        />
+        <span>{this.props.data.day}</span>
+      </StyledContainer>
+    );
+  }
+}
--- src/components/ChartView/ChartView.tsx
+++ src/components/ChartView/ChartView.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { Bar } from '../Bar/Bar';
+
 export const StyledDiv = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
   height: 200px;
   width: 100%;
   background-color: hsl(33, 100%, 98%);
@@ -17,6 +22,16 @@ export class ChartView extends React.Component<ChartViewProps, {}> {
   }
 
   render() {
-    return <StyledDiv></StyledDiv>;
+    return (
+      <StyledDiv>
+        <Bar data={this.props.data[0]} />
+        <Bar data={this.props.data[1]} />
+        <Bar data={this.props.data[2]} />
+        <Bar data={this.props.data[3]} />
+        <Bar data={this.props.data[4]} />
+        <Bar data={this.props.data[5]} />
+        <Bar data={this.props.data[6]} />
+      </StyledDiv>
+    );
   }
 }
